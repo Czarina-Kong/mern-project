34ed398cd1b3990fb6d24bb1aa49bae33b8befe2
Czarina-Kong <czarinakong@gmail.com>
2021-02-16 22:58:17 -0800

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello There</h1>
+    </div>
+  );
+}
+
+export default App;
